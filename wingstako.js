dc401d8029de7a2eebb00f5739094fa47e6e14a3
wingstako <wingstako@proton.me>
2024-03-11 17:06:07 -0700

    🐛 fix static file path in Neocities

--- wingstako.js
+++ wingstako.js
@@ -3,4 +3,16 @@ import dirTree from 'directory-tree';
 
 const tree = dirTree("static/directory", { attributes: ['type', 'extension', 'mode', 'mtime'] });
 
+// loop all nodes in the tree and remove `static/ or static\` from the path
+function removeStaticFromPath(node) {
+  if (node.path) {
+    node.path = node.path.replace('static/', '').replace('static\\', '');
+  }
+  if (node.children) {
+    node.children.forEach(removeStaticFromPath);
+  }
+}
+
+removeStaticFromPath(tree);
+
 fs.writeFileSync('static/directory.json', JSON.stringify(tree, null, 2));
